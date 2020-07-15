e3f860e8dfc493268284115f2cae3ec7f5162ca0
JC8747 <jonathan.chiaverini@gmail.com>
2020-07-14 21:09:25 -0400

    MVP completed

--- eating_cookies/eating_cookies.py
+++ eating_cookies/eating_cookies.py
@@ -2,13 +2,24 @@
 Input: an integer
 Returns: an integer
 '''
-def eating_cookies(n):
-    # Your code here
+def eating_cookies(cookies, noms=None):
+    if noms is None:
+        noms = {}
+    if cookies < 2:
+        return 1
+    elif cookies == 2:
+        return 2
+    elif cookies == 3:
+        return 4
+    elif noms and noms[cookies]:
+        return noms[cookies]
+    else:
+        noms[cookies] = eating_cookies(cookies-1, noms) + eating_cookies(cookies-2, noms) + eating_cookies(cookies-3, noms)
 
-    pass
+    return noms[cookies]
 
 if __name__ == "__main__":
     # Use the main function here to test out your implementation
     num_cookies = 5
 
-    print(f"There are {eating_cookies(num_cookies)} ways for Cookie Monster to each {num_cookies} cookies")
+    print(f"There are {eating_cookies(num_cookies)} noms for Cookie Monster to each {num_cookies} cookies")
